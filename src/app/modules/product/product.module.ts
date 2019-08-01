f13e8e77523e0eb487e69b634db0d2678b9bf016
Mouad <mouad.misrar@altreze.ma>
2019-08-01 18:35:19 +0100

    + Adding font awesome support + Removing filter pipe usage

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -3,7 +3,7 @@
  * @Date:   2019-07-31T13:34:35+01:00
  * @Filename: product.module.ts
  * @Last modified by:   mouad
- * @Last modified time: 2019-07-31T19:18:23+01:00
+ * @Last modified time: 2019-08-01T15:57:24+01:00
  * @Copyright: Altreze SARL
  */
 
@@ -13,13 +13,13 @@ import { FormsModule } from '@angular/forms';
 
 import { ProductRoutingModule } from './product-routing.module';
 import * as components from './components';
-import * as pipes from './pipes';
+// import * as pipes from './pipes';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
-    components.ProductsComponent,
-    pipes.FilterPipe
+    components.ProductsComponent
+    // pipes.FilterPipe
   ],
   imports: [
     CommonModule,
